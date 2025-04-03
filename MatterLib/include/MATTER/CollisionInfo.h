071f14f13b591a6bccd6ea8299cdb2b9cca63cc0
adrian <adrileroux9@gmail.com>
2025-04-03 20:07:37 +0200

    contact points vector

--- MatterLib/include/MATTER/CollisionInfo.h
+++ MatterLib/include/MATTER/CollisionInfo.h
@@ -8,16 +8,24 @@
 #include "MATTER/math/Vector2.h"
 
 struct CollisionInfo {
+private:
+    std::vector<Vector2f> contacts;
+
+public:
     bool collided;
     Vector2f normal;
     float depth;
-    std::vector<Vector2f> contacts;
 
     explicit CollisionInfo(const bool collided = false, const Vector2f& normal = {}, const float depth = 0.0f)
         : collided(collided), normal(normal), depth(depth) {}
 
     void addContact(const Vector2f& contact) {
-        contacts.push_back(contact);
+        if (contacts.size() < 2) contacts.push_back(contact);
+        else std::cerr << "A CollisionInfo can have only 2 contact points maximum !" << std::endl;
+    }
+
+    std::vector<Vector2f>& getContactPoints() {
+        return contacts;
     }
 };
 
